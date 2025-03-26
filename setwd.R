07418944243028d555af6f65dd3969ed3d7359d7
statnat <29431910+statnat@users.noreply.github.com>
2025-03-26 13:24:55 -0400

    remove wd

---
+++ setwd.R
@@ -0,0 +1,3 @@
+setwd("C:\\Users\\sumet\\OneDrive\\Desktop\\git\\oud_data")
+
+data.wd <- "C:\\Users\\sumet\\OneDrive\\Desktop\\onedrive\\OneDrive - University of Pittsburgh\\PHDL work\\OUD grant\\data"
