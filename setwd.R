e91b20d77419311caa3d4a28517bef8bc06535bb
statnat <29431910+statnat@users.noreply.github.com>
2025-03-26 13:39:15 -0400

    f

--- setwd.R
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-setwd("C:\\Users\\sumet\\OneDrive\\Desktop\\git\\oud_data")
-
-data.wd <- "C:\\Users\\sumet\\OneDrive\\Desktop\\onedrive\\OneDrive - University of Pittsburgh\\PHDL work\\OUD grant\\data"
-=======
-setwd("C:\\Users\\sumet\\OneDrive\\Desktop\\git\\oud_data")
->>>>>>> 4e2ab3910f8c0aaa4f09e79c8effbbca079dbfd0
